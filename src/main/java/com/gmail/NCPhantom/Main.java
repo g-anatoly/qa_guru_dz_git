21241f10d7cd5002eeee8385de98deeb084c284f
Its Bugs <NCPhantom@gmail.com>
2023-11-02 02:06:04 +0500

    added getMessage() methode4

--- src/main/java/com/gmail/NCPhantom/Main.java
+++ src/main/java/com/gmail/NCPhantom/Main.java
@@ -2,8 +2,9 @@ package com.gmail.NCPhantom;
 
 public class Main {
     public static void main(String[] args) {
-        
-        System.out.println("!!!Hello qa.guru!!!");
-        
+        System.out.println(getMessage());
+    }
+    public static String getMessage(){
+        return "!!!Hello qa.guru!!!";
     }
 }
